b566b3ea2988d914c4c7180a0031752589d2dba8
David Glick <davidglick@groundwire.org>
2011-04-30 03:53:05 +0000

    use stdlib doctest
    svn path=/plone.autoform/trunk/; revision=49178

--- plone/autoform/tests/test_doctests.py
+++ plone/autoform/tests/test_doctests.py
@@ -2,19 +2,19 @@ import unittest
 import doctest
 
 import zope.app.testing.placelesssetup
-import zope.testing.doctest
+
 
 def test_suite():
     return unittest.TestSuite((
-        zope.testing.doctest.DocFileSuite('../autoform.txt',
+        doctest.DocFileSuite('../autoform.txt',
             setUp=zope.app.testing.placelesssetup.setUp,
             tearDown=zope.app.testing.placelesssetup.tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
-        zope.testing.doctest.DocFileSuite('../view.txt',
+        doctest.DocFileSuite('../view.txt',
             setUp=zope.app.testing.placelesssetup.setUp,
             tearDown=zope.app.testing.placelesssetup.tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
-        zope.testing.doctest.DocFileSuite('../supermodel.txt',
+        doctest.DocFileSuite('../supermodel.txt',
             setUp=zope.app.testing.placelesssetup.setUp,
             tearDown=zope.app.testing.placelesssetup.tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,),
