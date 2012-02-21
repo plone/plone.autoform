76a46c66468fe87905e30805ee7c109db1ca1a7a
David Glick <dglick@gmail.com>
2012-02-20 19:15:12 -0800

    vb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '1.1'
+version = '1.1.1dev'
 
 long_description = (
     read('README.txt')
