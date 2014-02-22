8648ac7830d557de36444ac9eea558284a11de55
Timo Stollenwerk <contact@timostollenwerk.net>
2014-02-22 10:22:39 +0100

    Rename CHANGES.txt -> CHANGES.rst.

--- setup.py
+++ setup.py
@@ -11,42 +11,43 @@ version = '1.6.dev0'
 long_description = (
     read('README.rst')
     + '\n' +
-    read('CHANGES.txt')
+    read('CHANGES.rst')
     + '\n'
     )
 
-setup(name='plone.autoform',
-      version=version,
-      description="Tools to construct z3c.form forms",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.autoform',
+    version=version,
+    description="Tools to construct z3c.form forms",
+    long_description=long_description,
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone form z3c.form',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://code.google.com/p/dexterity',
-      license='LGPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.schema',
-          'zope.security',
-          'zope.dottedname',
-          'plone.supermodel>=1.1dev',
-          'plone.z3cform',
-          'z3c.form',
-          # 'AccessControl',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='plone form z3c.form',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='http://code.google.com/p/dexterity',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.schema',
+        'zope.security',
+        'zope.dottedname',
+        'plone.supermodel>=1.1dev',
+        'plone.z3cform',
+        'z3c.form',
+        # 'AccessControl',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
