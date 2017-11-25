e8b4faf66ab31a9d9f50409c334225aae18caae2
esteele <eric@esteele.net>
2017-11-24 20:30:52 -0500

    Back to development: 1.7.5
    [ci skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '1.7.4'
+version = '1.7.5.dev0'
 
 long_description = (read('README.rst') + '\n' + read('CHANGES.rst'))
 
