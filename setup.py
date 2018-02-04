bb066f112e15e64d669993d4754f8a13d1f67dab
Gil Forcada <gil.gnome@gmail.com>
2018-02-04 23:23:14 +0100

    Back to development: 1.7.6
    [ci-skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '1.7.5'
+version = '1.7.6.dev0'
 
 long_description = (read('README.rst') + '\n' + read('CHANGES.rst'))
 
